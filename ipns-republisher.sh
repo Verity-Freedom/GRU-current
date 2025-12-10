ae4149db034a91a78cbbe16eb88c403d2395ef63
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-10 07:59:13 +0300

    Update ipns-republisher.sh

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-./ld-linux-x86-64.so.2 --library-path . ./ipfs init
-./ld-linux-x86-64.so.2 --library-path . ./ipfs daemon &
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs init
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs daemon &
 sleep 10
-./ld-linux-x86-64.so.2 --library-path . ./ipfs routing get /ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 > record.bin
-curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4 -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/link > record.bin
+curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
