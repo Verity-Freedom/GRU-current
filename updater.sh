4fefbf9530327a703e75042e87dac654ad45a612
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-26 00:32:54 +0300

    Update updater.sh
    The same thing here.

--- updater.sh
+++ updater.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+ping -c 1 ipfs.io
+if [ $? -ne 0 ]; then
+read -p "I need ipfs.io connectivity to update. Please check your Internet connection."
+exit
+fi
 cp -r ./data ~/data
 rm -r *
 curl "https://ipfs.io/ipns/link/file.zip" -O
