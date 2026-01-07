79f38105667fc463e9b6d937ac7da7cb7d7370c2
Chara-Freedom <lolered@yandex.ru>
2026-01-07 20:33:45 +0300

    Update updater.sh
    Safety check

--- updater.sh
+++ updater.sh
@@ -5,6 +5,12 @@ if [ $? -ne 0 ]; then
 read -p "I need ipfs.io connectivity to update. Please check your Internet connection."
 exit
 fi
+if [ $(ls -1 | wc -l) -gt 18 ]; then
+read -n 1 -p "There are too many files to update. You don't want to run the updater in a folder with your personal files. Press any key if you want to exit or 0 if you want to update anyway." INP
+ if [ $INP != 0 ]; then
+ exit
+ fi
+fi
 cp -r ./data ~/data
 rm -r *
 curl "https://ipfs.io/ipns/link/file.zip" -O
