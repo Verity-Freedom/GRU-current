a62ec248aee71fc002c8cf6b1fae02bb4ec1c860
Chara-Freedom <lolered@yandex.ru>
2026-02-09 04:28:29 +0300

    Update autoupdater-service.sh

--- autoupdater-service.sh
+++ autoupdater-service.sh
@@ -3,7 +3,8 @@ cd "$(dirname "$0")"
 UPD=(VERSION*)
 curl "https://ipfs.io/ipns/link/$UPD" -f -s -o /dev/null
 if [ $? -eq 22 ]; then
-read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to skip" INP
+read -n 1 -p "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Press any key if you want to update or 0 to skip " INP
+echo
 systemctl --user is-active --quiet .service
  if [ $? -eq 0 ]; then
  CHECK=0
