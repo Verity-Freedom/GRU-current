96dfd7b6469aa76e80eebd2ff50c6ac4befbe1ec
Chara-Freedom <rabcristine@proton.me>
2026-02-13 03:38:56 +0300

    Update ipns-republisher.sh

--- ipns-republisher.sh
+++ ipns-republisher.sh
@@ -5,9 +5,9 @@ cd "$(dirname "$0")"
 sleep 20
 while pgrep -x ld-linux-x86-64 > /dev/null
 do
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/link > record.bin
-curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
-./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs routing get /ipns/link2 > record.bin
-curl -i -X PUT https://delegated-ipfs.dev/routing/v1/ipns/link2 -H "Content-Type: application/vnd.ipfs.ipns-record" --data-binary @record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/link1 > record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put link1 record.bin --force
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name get /ipns/link2 > record.bin
+./ld-linux-x86-64.so.2 --library-path . ../kubo/ipfs name put link2 record.bin --force
 sleep 1d
 done
