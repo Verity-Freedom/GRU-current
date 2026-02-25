2cfb188222d72d2cfe3e61055658ce70d5f17cfc
Chara-Freedom <lolered@yandex.ru>
2026-02-25 09:42:41 +0300

    Update updater.sh
    Syntax consistency

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 curl "https://ipfs.io/ipns/link/test.txt" -f -s -o /dev/null
-if [ $? -eq 22 ]; then
+if [ $? = 22 ]; then
 read -p "I need ipfs.io connectivity to update. Please check your Internet connection. "
 exit
 fi
